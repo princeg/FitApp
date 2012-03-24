ecefe205767c19f29304ea0bc081b474dc030943
princeg <prince.coer@gmail.com>
2012-03-24 12:59:02 -0700

    More media, and login screen

--- apps/sdapp/sdapp/gen/com/android/sdapp/R.java
+++ apps/sdapp/sdapp/gen/com/android/sdapp/R.java
@@ -11,26 +11,29 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int loginbutton=0x7f020002;
+        public static final int welcome=0x7f020003;
     }
     public static final class id {
         public static final int button1=0x7f05000d;
         public static final int button2=0x7f05000e;
-        public static final int editText1=0x7f050001;
-        public static final int editText2=0x7f050002;
-        public static final int seekBar1=0x7f050009;
-        public static final int spinner1=0x7f05000c;
+        public static final int editText1=0x7f05000b;
+        public static final int editText2=0x7f05000c;
+        public static final int seekBar1=0x7f050005;
+        public static final int spinner1=0x7f05000a;
         public static final int textView1=0x7f050003;
-        public static final int textView2=0x7f050004;
-        public static final int textView3=0x7f050005;
-        public static final int textView4=0x7f05000a;
-        public static final int textView5=0x7f05000b;
+        public static final int textView2=0x7f050007;
+        public static final int textView3=0x7f050006;
+        public static final int textView4=0x7f050008;
+        public static final int textView5=0x7f050009;
         public static final int textView6=0x7f05000f;
         public static final int textView7=0x7f050010;
         public static final int widget0=0x7f050000;
-        public static final int widget35=0x7f050007;
-        public static final int widget36=0x7f050006;
-        public static final int widget71=0x7f050008;
+        public static final int widget35=0x7f050002;
+        public static final int widget36=0x7f050001;
+        public static final int widget71=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
